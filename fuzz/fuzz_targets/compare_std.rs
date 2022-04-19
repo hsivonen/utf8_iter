a69267209619661104557ced048f9766454ceb5f
Henri Sivonen <hsivonen@hsivonen.fi>
2022-04-19 11:33:32 +0300

    Initial release

---
+++ fuzz/fuzz_targets/compare_std.rs
@@ -0,0 +1,9 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use utf8_iter::Utf8Chars;
+
+fuzz_target!(|data: &[u8]| {
+    let from_iter: String = Utf8Chars::new(data).collect();
+    let from_std = String::from_utf8_lossy(data);
+    assert_eq!(from_iter, from_std);
+});
